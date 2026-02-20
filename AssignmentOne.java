26ca21e832895cff50583ce3fb2295e3414841d7
GrahamLord04 <grahamlord04@gmail.com>
2026-02-20 11:16:25 -0600

    Initial Commit

---
+++ AssignmentOne.java
@@ -0,0 +1,17 @@
+public class AssignmentOne {
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        System.out.println("Task One To Be Implemented");
+    }
+
+    public static void restockItem(String[] names, String target, int amount) {
+        System.out.println("Task Two To Be Implemented");
+    }
+
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        System.out.println("Task Three To Be Implemented");
+    }
+}
